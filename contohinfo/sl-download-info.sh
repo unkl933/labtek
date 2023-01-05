e30b8afb185c77a6c98eaaf3ed8fea030d51046d
dopamine <crack1933@gmail.com>
2023-01-06 04:05:58 +0700

    Update sl-download-info.sh

--- contohinfo/sl-download-info.sh
+++ contohinfo/sl-download-info.sh
@@ -19,7 +19,7 @@ wget -O info-exra-unli "https://raw.githubusercontent.com/unkl933/labtek/labtek/
 
 wget -O info-status-respon "https://raw.githubusercontent.com/unkl933/labtek/labtek/contohinfo/info-status-respon.sh"
 
-wget -O info-tsel-opok "https://raw.githubusercontent.com/unkl933/labtek/labtek/main/contohinfo/info-tsel-opok.sh"
+wget -O info-tsel-opok "https://raw.githubusercontent.com/unkl933/labtek/labtek/contohinfo/info-tsel-opok.sh"
 
 chmod +x info-cek-tersedia
 chmod +x info-contoh-payload
